a234a7b0136f1caeae24bb3788f3090d1078cc04
Aakash Mishra <aakashmishra.281@gmail.com>
2023-05-31 13:11:04 +0530

    Error Modal when signup twice same email

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,11 +5,14 @@ import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../s
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 
 const Auth = () => {
     const auth = useContext(AuthContext);
 
     const [isLogin, setLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
     const [formState, inputHandler, setFormData] = useForm(
         {
             email: {
@@ -52,7 +55,9 @@ const Auth = () => {
 
         } else {
             try {
-                const response = await fetch('https://localhost:5000/api/users/signup', {
+                setIsLoading(true);
+
+                const response = await fetch('http://localhost:5000/api/users/signup', {
                     method: 'POST',
                     headers: {
                         //without backend won't know what kind of data it recieves. our body parser will not kick in correctly.
@@ -66,56 +71,73 @@ const Auth = () => {
                         password: formState.inputs.password.value
                     })
                 });
+
                 const responseData = await response.json();
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+                //if we throw the error, the code below will not execute.
                 console.log(responseData);
+                setIsLoading(false);
+                auth.login();
             } catch (err) {
                 console.log(err);
+                setIsLoading(false);
+                setError(err.message || 'Something went wrong, please try again.');
             }
         }
-        auth.login();
+
     };
+    //line 86 to set the erro modal.
+    //to reset the error
+    const errorHandler = () => { setError(null); }
+
     return (
-        <Card className="authentication">
-            <h2>Login required</h2>
-            <hr />
-            <form onSubmit={userSubmitHandler}>
-                {!isLogin && (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            <Card className="authentication">
+
+                <h2>Login required</h2>
+                <hr />
+                <form onSubmit={userSubmitHandler}>
+                    {!isLogin && (
+                        <Input
+                            id="name"
+                            type="text"
+                            element="input"
+                            label="Your Name. "
+                            validators={[VALIDATOR_REQUIRE()]}
+                            errorText="Please enter a name ."
+                            onInput={inputHandler}
+                        />
+                    )}
                     <Input
-                        id="name"
+                        id="email"
                         type="text"
                         element="input"
-                        label="Your Name. "
-                        validators={[VALIDATOR_REQUIRE()]}
-                        errorText="Please enter a name ."
+                        validators={[VALIDATOR_EMAIL()]}
+                        label="Email"
+                        errorText="Please enter a valid email address."
                         onInput={inputHandler}
                     />
-                )}
-                <Input
-                    id="email"
-                    type="text"
-                    element="input"
-                    validators={[VALIDATOR_EMAIL()]}
-                    label="Email"
-                    errorText="Please enter a valid email address."
-                    onInput={inputHandler}
-                />
-                <Input
-                    id="password"
-                    type="password"
-                    element="input"
-                    label="Password"
-                    validators={[VALIDATOR_MINLENGTH(6)]}
-                    errorText="Please enter a valid password. Minimum length = 6. "
-                    onInput={inputHandler}
-                />
-                <Button type="submit" disabled={!formState.isValid}>
-                    {isLogin ? 'LOGIN' : 'SIGNUP'}
+                    <Input
+                        id="password"
+                        type="password"
+                        element="input"
+                        label="Password"
+                        validators={[VALIDATOR_MINLENGTH(6)]}
+                        errorText="Please enter a valid password. Minimum length = 6. "
+                        onInput={inputHandler}
+                    />
+                    <Button type="submit" disabled={!formState.isValid}>
+                        {isLogin ? 'LOGIN' : 'SIGNUP'}
+                    </Button>
+                </form>
+                <Button inverse onClick={switchModeHandler}>
+                    SWITCH TO {isLogin ? 'SIGNUP' : 'LOGIN'}
                 </Button>
-            </form>
-            <Button inverse onClick={switchModeHandler}>
-                SWITCH TO {isLogin ? 'SIGNUP' : 'LOGIN'}
-            </Button>
-        </Card>
+            </Card>
+        </React.Fragment>
     )
 
 }
